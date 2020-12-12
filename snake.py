cc3fd8de47f6971488f2a49fdac1fe3370d68b21
dairon0919 <daironrocks@gmail.com>
2020-12-11 19:22:34 -0500

    food and head

--- snake.py
+++ snake.py
@@ -1,47 +1,37 @@
-"see https://www.geeksforgeeks.org/create-a-snake-game-using-turtle-in-python/ for tutorial"
-import turtle 
-import time 
-import random 
-  
+"see https://pastebin.com/hnpyWyVz for tutorial"
+
+import turtle
+import time
+import random
+ 
 delay = 0.1
+ 
+# Score
 score = 0
 high_score = 0
-
-# Creating a window screen 
-wn = turtle.Screen() 
-wn.title("Snake Game") 
-wn.bgcolor("blue") 
-  
-# the width and height can be put as user's choice 
-wn.setup(width=600, height=600) 
-wn.tracer(0) 
-  
-  
-# head of the snake 
-head = turtle.Turtle() 
-head.shape("square") 
-head.color("red") 
-head.penup() 
-head.goto(0,0) 
-head.direction = "Stop"
-  
-  
-# food in the game 
-food = turtle.Turtle() 
-colors = random.choice(['red', 'green', 'black']) 
-shapes = random.choice(['square', 'triangle', 'circle']) 
-food.speed(0) 
-food.shape(shapes) 
-food.color(colors) 
-food.penup() 
-food.goto(0, 100) 
-
-pen = turtle.Turtle() 
-pen.speed(0) 
-pen.shape("square") 
-pen.color("white") 
-pen.penup() 
-pen.hideturtle() 
-pen.goto(0, 250) 
-pen.write("Score : 0  High Score : 0", align="center", 
-          font=("candara", 24, "bold")) 
+ 
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("gold")
+wn.setup(width=600, height=400)
+wn.tracer(0) # Turns off the screen updates
+ 
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("Purple")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+ 
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("white")
+food.penup()
+food.goto(0,100)
+ 
+segments = []
+wn.mainloop()
